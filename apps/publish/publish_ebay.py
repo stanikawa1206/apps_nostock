2912d975c0a4b6f4fa7bd739ddf24c438c4f1632
Shinichiro Tanikawa <info@hrsp.co.jp>
2026-03-07 08:37:39 +0900

    auto deploy

--- apps/publish/publish_ebay.py
+++ apps/publish/publish_ebay.py
@@ -1583,6 +1583,7 @@ def fetch_accounts_for_pc(conn, current_pc):
     extra = [
         ("谷川③","x210-131-209-232","C", 1000),
         ("谷川②","x210-131-209-103","C", 1000),
+        ("川島","x85-131-251-127","C", 1000),
         ("谷川③","mouse","C", 1000),]
 
     for account, pc, preset_group, target in extra:
