5a669fdf367a472f718ea60738b24d93df0e4b8b
Shinichiro Tanikawa <info@hrsp.co.jp>
2026-03-22 06:05:51 +0900

    auto deploy

--- apps/inventory/check_remaining_ebay.py
+++ apps/inventory/check_remaining_ebay.py
@@ -401,6 +401,7 @@ def run_remaining_worker(worker_name: str):
             )
 
             while processed_count < MAX_PER_RUN:
+                print("1にしてみた")
                 rows = pull_remaining_targets(pull_conn, worker_name, batch_size=1)
 
                 if not rows:
