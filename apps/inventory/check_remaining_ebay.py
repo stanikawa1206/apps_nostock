4c0b54ca17617d9b217b0b4fb24e56eb75b7f782
Shinichiro Tanikawa <info@hrsp.co.jp>
2026-03-24 06:47:13 +0900

    auto deploy

--- apps/inventory/check_remaining_ebay.py
+++ apps/inventory/check_remaining_ebay.py
@@ -439,7 +439,8 @@ def run_remaining_worker(worker_name: str):
                     print(f"\n[INFO] batch processing {processed_count + 1}/{MAX_PER_RUN} ...")
                     # ★ここで毎回 new_page 26/3/26
                     page = context.new_page()
-                    # リクエストを制御して軽量化してる処理
+                    # 画像等の通信を遮断-リクエストを制御して軽量化してる処理 →　必要な通信まで停める可能性あり　だからコメントアウト
+                    print("画像等の通信を遮断 をcomment out")
                     # page.route("**/*", lambda route: 
                     #    route.abort() if route.request.resource_type in ["image", "media", "font", "stylesheet"] 
                     #    else route.continue_()
